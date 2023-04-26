2ef5ed53f00299a884d6b2e8574e9981e1d6aada
LuisC92 <luis.clara@wildcodeschool.com>
2023-04-26 11:33:28 +0100

    frontend file upload repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
+import {storage} from "./firebase"
+import {ref, uploadBytes, getDownloadURL} from "firebase/storage"
+import axios from "axios"
 
 function App() {
+
+  const [image, setImage] = useState()
+  const [url, setUrl] = useState()
+  const [counter, setCounter] = useState(1)
+
+  const handleChange = (event)=>{
+    // console.log(event.target.files[0])
+    setImage(event.target.files[0])
+  }
+
+  const createImage = (imageObject) =>{
+    axios.post("http://localhost:8000/images", imageObject)
+      .then(response => console.log(response))
+  }
+
+  const handleSubmit = (event)=>{
+    let count = counter
+    event.preventDefault()
+    const imageRef = ref(storage, `1-profile-image-${counter}`)
+    uploadBytes(imageRef, image)
+      .then(()=>{
+        getDownloadURL(imageRef)
+          .then(url => {
+            const imageObject ={
+              imageUrl:url
+            }
+            createImage(imageObject)
+            setCounter(count += 1)
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>File Upload</h1>
+      <form onSubmit={handleSubmit}> 
+        <label>Picture: </label>
+        <br/>
+        <br/>
+        <input type="file" onChange={handleChange} />
+        <br/>
+        <br/>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
